7cda1da201312ce248b2e44865cf087067def9ff
Wakjira Bekele <73541693+wakjirabekele@users.noreply.github.com>
2023-10-25 10:03:51 +0300

    Update 2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,65 +1,30 @@
 #!/usr/bin/env python3
-"""Base Caching module
+"""
+Implements caching
 """
 
-from base_caching import BaseCaching
+BaseCaching = __import__("base_caching").BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """ BaseCaching:
-      - Simple class to implement a cache system
-      - Learn the basis to cache algorithnms
-    """
-
+    """implements the LIFO caching algorithm"""
     def __init__(self):
-        """
-        CONSTRUCTOR
-        """
         super().__init__()
-        self.__last_update: str
+        self.last_key = ""
 
     def put(self, key, item):
-        """
-        update the key in the cache data
-        """
-
-        def IsCacheFull() -> bool:
-            """ Verify is cached it's full filled"""
-            if len(self.cache_data) == self.MAX_ITEMS:
-                return True
-            return False
-
-        def IsNewKey(key) -> bool:
-            """ Verify if the key to add to cache is new or already exists"""
-            keys = [x for x in self.cache_data]
-            if key in keys:
-                return False
-            return True
-
-        def PopLast():
-            """deletes the last item in to cache"""
-            self.cache_data.pop(self.__last_update)
-
-        def AddToCache(key, item):
-            """add or update a key in cache"""
-            self.cache_data.update({key: item})
-
-        def UpdateLast(key):
-            """update the control var last_update"""
-            self.__last_update = key
-
-        isFull: bool = IsCacheFull()
-        isNew: bool = IsNewKey(key)
-        if isFull:
-            if isNew:
-                print('DISCARD: {}'.format(self.__last_update))
-                PopLast()
-
-        AddToCache(key, item)
-        UpdateLast(key)
+        """ Adds an item to the dictinary"""
+        if key and item:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                print("DISCARD: {}".format(self.last_key))
+                self.cache_data.pop(self.last_key)
+            self.last_key = key
 
     def get(self, key):
+        """accesses a dictinary item using a key and
+           returns it if it exists
         """
-        Return the valur from the given key if exists
-        """
+        if key is None or key not in self.cache_data.keys():
+            return None
         return self.cache_data.get(key)
